c34492aa03ad75a2eb2efd38b6ffe0bc2d45c781
JoaoHenriqueBarbosa <joaohenriquebarbosa21@gmail.com>
2021-02-13 23:41:47 -0300

    Concertando problemas anteriores

--- src/JanxForm.js
+++ src/JanxForm.js
@@ -178,16 +178,6 @@ const JanxForm = forwardRef(
         return getFieldValue(fieldName);
       },
 
-      setFieldValue(fieldName, value) { // IMPLEMENT skipValidation?
-        const field = getField(fieldName);
-
-        if (!field) {
-          return false;
-        }
-
-        return setFieldValue(field, value);
-      },
-
       getField(fieldName) {
         return getField(fieldName)
       },
